8c8525a50315a4a3adcd1eba989af41b42d1b549
joallisson <joallissonnc@gmail.com>
2023-09-13 15:55:23 -0300

    Finalizado o backend do NLW

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
 import {fastify} from 'fastify'
 import { getAllPromptRoute } from './routes/get-all-prompts'
+import { uploadVideoLoad } from './routes/upload-video'
+import { createTranscriptionRoute } from './routes/create-transcription'
+import { generateAICompletionRoute } from './routes/generate-ai-completion'
+import { fastifyCors } from '@fastify/cors'
 
 const app = fastify()
 
+app.register(
+    fastifyCors,
+    {
+        origin: '*'
+    }
+)
+
 app.register(getAllPromptRoute)
+app.register(uploadVideoLoad)
+app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
 
 app.listen({
     port: 3333
 }).then(() => {
-    console.log('HTTP Server Running!, port 3333')
+    console.log('HTTP Server Running on port: 3333')
 })
